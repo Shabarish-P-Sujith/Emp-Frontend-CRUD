0e15714dc4d2f1abd23be5149e42e12e0873a685
Shabarish-P-Sujith <shabarishpsujith@gmail.com>
2025-06-03 11:22:10 +0530

    all files v2

--- my-empcrud-frontend/src/components/EmployeeList.jsx
+++ my-empcrud-frontend/src/components/EmployeeList.jsx
@@ -14,12 +14,12 @@ function EmployeeList() {
   }, []);
 
   const fetchEmployees = () => {
-    axios.get('http://localhost:3001/employees')
+    axios.get('http://emp-backend-crud.onrender.com/employees')
       .then(res => setEmployees(res.data));
   };
 
   const handleDelete = (id) => {
-    axios.delete(`http://localhost:3001/employees/${id}`).then(() => {
+    axios.delete(`http://emp-backend-crud.onrender.com/employees/${id}`).then(() => {
       setEmployees(employees.filter(emp => emp.id !== id));
       toast.success('Employee deleted successfully!');
     });
